0804d4aa61b7162b6097da05410bce2c93dc884c
Ulin70 <141465512+Ulin70@users.noreply.github.com>
2024-09-10 11:10:37 +0700

    Add files via upload

--- alex.js
+++ alex.js
@@ -1,1 +1,35 @@
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const { Client, LegacySessionAuth, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
+// Buat instance baru dari Client dengan LocalAuth sebagai strategi otentikasi
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: 'client-one' // Identifikasi unik untuk menyimpan sesi
+    })
+});
+
+// Simpan sesi ke file setelah berhasil otentikasi
+client.on('authenticated', (session) => {
+    console.log(session);
+});
+
+// Inisialisasi client
+client.initialize();
+
+// Menangani event saat QR code dihasilkan
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+// Menangani event saat client siap untuk mengirim dan menerima pesan
+client.on('ready', () => {
+    console.log("Client siap untuk mengirim dan menerima pesan");
+});
+
+client.on('message', message => {
+    if(message.body === 'halo')
+        {
+            message.reply('Hai');
+        }
+});
